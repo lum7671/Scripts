276752dce460cf80e7bef4d550f171517e2f81ab
장두현(Doohyun Jang)/Data Engineering팀/SKP <nate.jang@sk.com>
2021-08-26 07:08:13 +0900

    route 테이블 정리.

---
+++ del_route_ips.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+sudo route -n delete 172.30.1.0/27
+sudo route -n delete 172.30.1.29
+sudo route -n delete 172.30.1.32
+sudo route -n delete 172.30.1.48
+sudo route -n delete 172.30.1.56
+sudo route -n delete 172.30.1.58
+sudo route -n delete 172.30.1.59
+sudo route -n delete 172.30.1.60
+sudo route -n delete 172.30.1.64
+sudo route -n delete 172.30.1.128
+sudo route -n delete 172.30.1.192
+sudo route -n delete 172.30.1.224
+sudo route -n delete 172.30.1.240
+sudo route -n delete 172.30.1.248
+sudo route -n delete 172.30.1.252
+sudo route -n delete 172.30.1.255
