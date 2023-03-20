9b25b7b7db5caa03a854df3be6a8648a00dea1b6
장두현(Doohyun Jang)/Data Engineering팀/SKP <nate.jang@sk.com>
2023-03-20 12:12:26 +0900

    - git 목록 추가 - rust update 추가 - 코드 리펙토링 hard code 된 부분을 $HOME 사용

--- update_all.zsh
+++ update_all.zsh
@@ -7,12 +7,28 @@ GITS=(
 #    "/Users/x/.oh-my-zsh/custom/themes/agkozak/"
 #    "/Users/x/.oh-my-zsh/custom/themes/spaceship-prompt/"
 #    "/Users/x/.zgenom"
+    "$HOME/git/Flex/"
     "$HOME/git/MacOS-All-In-One-Update-Script/"
     "$HOME/git/Scripts/"
-    "$HOME/git/prelude/"
+    "$HOME/git/lum7671blog/"
+    "$HOME/git/pelican-plugins/"
+#    "$HOME/git/prelude/"
+    "$HOME/git/python-language-server/"
+    "$HOME/git/Update_All/"
+    "$HOME/git/Hyperpipe/"
+    "$HOME/git/dotfiles/"
+    "$HOME/git/ff2mpv/"
+    "$HOME/git/mopster/"
+    "$HOME/git/my-dotfiles/"
+    "$HOME/git/org-mode/"
+    "$HOME/git/worg/"
+    "$HOME/git/ytfzf/"
 )
 
 (
+    echo "==================================> update all rust packages"
+    cargo install-update -a
+
     echo "==================================> update git repository"
     for gp in "${GITS[@]}"
     do
@@ -21,12 +37,16 @@ GITS=(
         git pull
         git fetch --all --prune --jobs=10
     done
+
     echo "==================================> run update-all.sh"
     # [andmpel/MacOS-All-In-One-Update-Script: Mac update shell script (Appstore, macOS, Homebrew and others)](https://github.com/andmpel/MacOS-All-In-One-Update-Script)
     /usr/local/bin/zsh $HOME/git/MacOS-All-In-One-Update-Script/update-all.sh
-    # echo "==================================> Update_All.command"
-    # TERM=xterm bash /Users/x/git/Update_All/Update_All.command
+
+    echo "==================================> Update_All.command"
+    # [albinoz/Update_All: macOS Applications & System Update](https://github.com/albinoz/Update_All)
+    TERM=xterm bash "$HOME/git/Update_All/Update_All.command"
     # echo "==================================> update"
-    # TERM=xterm bash /Users/x/git/update/bin/update
+    # TERM=xterm bash "$HOME/git/update/bin/update"
+
     echo "==================================> The End !!!"
 ) > $LOGFILE 2>&1
