0ac0e8442a7eb4b1e922bbb59f17ce8fcf7dbd56
장두현(Doohyun Jang)/Data Engineering팀/SKP <nate.jang@sk.com>
2023-03-25 22:03:48 +0900

    comment 추가

--- JoplinBackup.zsh
+++ JoplinBackup.zsh
@@ -9,9 +9,11 @@ TMP_PATH="$HOME/tmp"
 cd $HOME
 echo "*** Start ***"
 echo "*** Make backup file in tmp directory ***"
+# [atool home](https://www.nongnu.org/atool/)
 apack $TMP_PATH/$TXZ_FILENAME Joplin
 echo "*** Move tmp file to backup directory ***"
 mv -v $TMP_PATH/$TXZ_FILENAME $BACKUP_PATH
 echo "*** Delete backup files older than 7 days ***"
+# [sharkdp/fd: A simple, fast and user-friendly alternative to 'find'](https://github.com/sharkdp/fd)
 fd -H 'txz$' --base-directory $BACKUP_PATH -tf --change-older-than 1weeks -X rm -vf
 echo "*** The End ***"
