bcf1dce6b00c3889207a5ec75d6b0b47e6377417
장두현(Doohyun Jang)/Data Platform개발팀/SKP <nate.jang@sk.com>
2025-04-08 17:15:57 +0900

    로그인 환경 추가 리펙토링 적용

--- update_all.zsh
+++ update_all.zsh
@@ -1,18 +1,19 @@
-#!/usr/bin/env zsh -li
+#!/usr/bin/env zsh
+# 로그인 쉘 환경 초기화를 위한 설정
+emulate -L zsh
+source ~/.zshrc
 
+export PATH="$HOME/opt/bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 
-TODAY="$(date +%Y%m%d_%H%M%S)"
-LOGFILE="/private/tmp/update_all-${TODAY}.log"
-GITS=(
-#    "/Users/x/.oh-my-zsh/custom/themes/agkozak/"
-#    "/Users/x/.oh-my-zsh/custom/themes/spaceship-prompt/"
-#    "/Users/x/.zgenom"
+# ===== CONFIGURATION =====
+readonly TODAY="$(date +%Y%m%d_%H%M%S)"
+readonly LOGFILE="/private/tmp/update_all-${TODAY}.log"
+readonly GITS=(
     "$HOME/git/Flex/"
     "$HOME/git/MacOS-All-In-One-Update-Script/"
     "$HOME/git/Scripts/"
     "$HOME/git/lum7671blog/"
     "$HOME/git/pelican-plugins/"
-#    "$HOME/git/prelude/"
     "$HOME/git/python-language-server/"
     "$HOME/git/Update_All/"
     "$HOME/git/Hyperpipe/"
@@ -23,36 +24,61 @@ GITS=(
     "$HOME/git/org-mode/"
     "$HOME/git/worg/"
     "$HOME/git/ytfzf/"
-#    "$HOME/git/emacs-mac/"
-#    "$HOME/git/doomemacs/"
     "$HOME/git/minutes_diff/"
 )
 
-(
-    echo "==================================> update all rust packages"
+# ===== FUNCTIONS =====
+info() {
+    echo "==================================>" "$@"
+}
+
+update_rust_packages() {
+    info "update all rust packages"
     cargo install-update -a
+}
 
-    echo "==================================> update git repository"
-    for gp in "${GITS[@]}"
-    do
-        echo "$gp updating..."
-        cd $gp
-        git pull
-        git fetch --all --prune --jobs=10
+update_git_repos() {
+    info "update git repositories"
+    local current_dir=$(pwd)
+    
+    for repo in "${GITS[@]}"; do
+        if [[ -d "$repo" ]]; then
+            echo "Updating $repo..."
+            cd "$repo"
+            git pull
+            git fetch --all --prune --jobs=10
+        else
+            echo "Warning: Directory not found - $repo"
+        fi
     done
+    
+    cd "$current_dir"
+}
 
-    echo "==================================> run update-all.sh"
-    # [andmpel/MacOS-All-In-One-Update-Script: Mac update shell script (Appstore, macOS, Homebrew and others)](https://github.com/andmpel/MacOS-All-In-One-Update-Script)
-    /usr/local/bin/zsh $HOME/git/MacOS-All-In-One-Update-Script/update-all.sh
+run_system_updates() {
+    info "run update-all.sh"
+    source "$HOME/git/MacOS-All-In-One-Update-Script/update-all.sh"
+    update-brew
+    update-atom
+    update-npm
+    update-gem
+    update-pip3
+    update-app_store
+    update-office
+    update-macos
 
-    echo "==================================> Update_All.command"
-    # [albinoz/Update_All: macOS Applications & System Update](https://github.com/albinoz/Update_All)
+    info "Update_All.command"
     TERM=xterm bash "$HOME/git/Update_All/Update_All.command"
-    # echo "==================================> update"
-    # TERM=xterm bash "$HOME/git/update/bin/update"
+}
 
-    # echo "==================================> JoplinBackup.zsh"
-    # /usr/local/bin/zsh $HOME/git/Scripts/JoplinBackup.zsh
+# ===== MAIN =====
+main() {
+    (
+        update_rust_packages
+        update_git_repos
+        run_system_updates
+        info "The End !!!"
+    ) > "$LOGFILE" 2>&1
+}
 
-    echo "==================================> The End !!!"
-) > $LOGFILE 2>&1
+main "$@"
